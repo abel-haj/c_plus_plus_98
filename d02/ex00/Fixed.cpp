70ba1e15454133dc8346e17f8986f2822839e808
abel-haj <abel-haj@student.1337.ma>
2021-12-21 21:00:04 +0100

    understanding const, copy constructor, assignment operator overload

---
+++ d02/ex00/Fixed.cpp
@@ -0,0 +1,45 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed() {
+
+	std::cout << "Default constructor called" << std::endl;
+	this->_value = 0;
+}
+
+/*
+ * copy constructor
+ */
+Fixed::Fixed( const Fixed& f ) {
+
+	std::cout << "Copy constructor called" << std::endl;
+	this->_value = f.getRawBits();
+}
+
+/*
+ * assignation operator overload
+ */
+Fixed& Fixed::operator=( Fixed f ) {
+
+	std::cout << "Assignation operator called" << std::endl;
+	_value = f.getRawBits();
+
+	// return *this;
+}
+
+int Fixed::getRawBits( void ) const {
+
+	std::cout << "getRawBits member function called" << std::endl;
+
+	return _value;
+}
+
+void Fixed::setRawBits( int const raw ) {
+
+	std::cout << "setRawBits member function called" << std::endl;
+	_value = raw;
+}
+
+Fixed::~Fixed() {
+
+	std::cout << "Destructor called" << std::endl;
+}
---
+++ d02/ex00/Fixed.hpp
@@ -0,0 +1,26 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed
+{
+	private:
+		int	_value;
+		static const int _bits;
+
+	public:
+		/* default constructor */
+		Fixed();
+		/* copy constructor */
+		Fixed( const Fixed& f );
+		/* assignation operator overload */
+		Fixed& operator=( Fixed f );
+		int getRawBits( void ) const;
+		void setRawBits( int const raw );
+		/* destructor */
+		~Fixed();
+};
+
+
+#endif
