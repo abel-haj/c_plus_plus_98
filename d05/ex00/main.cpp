705d2cbc045ff97ce24ba355753ba214967c6246
abel-haj <abel-haj@student.1337.ma>
2022-02-23 16:28:27 +0100

    some tests

--- d05/ex00/main.cpp
+++ d05/ex00/main.cpp
@@ -4,15 +4,45 @@ int	main () {
 
 	// Bureaucrat a;
 	// Bureaucrat b ( 100 );
-	try {
-
-		Bureaucrat c ( " ", 200 );
+	try
+	{
+		Bureaucrat c ( " ", 0 );
 	}
-	catch ( std::exception& e ) {
-
-		std::cout << e.what();
+	catch ( std::exception& e )
+	{
+		std::cout << e.what() << "\n\n";
 	}
-	// Bureaucrat c ( -5 );
+
+	try
+	{
+		Bureaucrat c ( " ", 151 );
+	}
+	catch ( std::exception& e )
+	{
+		std::cout << e.what() << "\n\n";
+	}
+
+	try
+	{
+		Bureaucrat a ( "random", 1 );
+		a.incrementGrade();
+	}
+	catch( std::exception& e )
+	{
+		std::cout << e.what() << "\n\n";
+	}
+
+	try
+	{
+		Bureaucrat a ( "random", 150 );
+		a.decrementGrade();
+	}
+	catch( std::exception& e )
+	{
+		std::cout << e.what() << "\n\n";
+	}
+
+	// Bureaucrat c ( "", -5 );
 
 	return 0;
 }
