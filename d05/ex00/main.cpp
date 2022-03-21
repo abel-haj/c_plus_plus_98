7fe8dc7061ed305b28e1b083d7de279d38d28f51
abel-haj <abel-haj@student.1337.ma>
2022-03-22 00:39:33 +0100

    proper tests

--- d05/ex00/main.cpp
+++ d05/ex00/main.cpp
@@ -1,48 +1,73 @@
 #include "Bureaucrat.hpp"
 
+#define NEWLINE std::cout << std::endl;
+
 int	main () {
 
-	// Bureaucrat a;
-	// Bureaucrat b ( 100 );
+	{
+		Bureaucrat a;
+		Bureaucrat b ( "Bob", 100 );
+		NEWLINE
+		b.incrementGrade();
+		NEWLINE
+		std::cout << b << std::endl;
+		NEWLINE
+		b.decrementGrade();
+		NEWLINE
+	}
+	NEWLINE
+
+	std::cout << "-------------" << std::endl;
+
 	try
 	{
-		Bureaucrat c ( " ", 0 );
+		Bureaucrat c ( "B", 0 );
 	}
 	catch ( std::exception& e )
 	{
-		std::cout << e.what() << "\n\n";
+		std::cout << e.what();
+		NEWLINE
 	}
+
+	std::cout << "-------------" << std::endl;
 
 	try
 	{
-		Bureaucrat c ( " ", 151 );
+		Bureaucrat c ( "", 151 );
 	}
 	catch ( std::exception& e )
 	{
-		std::cout << e.what() << "\n\n";
+		std::cout << e.what();
+		NEWLINE
 	}
+
+	std::cout << "-------------" << std::endl;
 
 	try
 	{
-		Bureaucrat a ( "random", 1 );
-		a.incrementGrade();
+		Bureaucrat c ( "random", 1 );
+		c.incrementGrade();
+		std::cout << "something\n";
 	}
 	catch( std::exception& e )
 	{
-		std::cout << e.what() << "\n\n";
+		std::cout << e.what();
+		NEWLINE
 	}
+
+	std::cout << "-------------" << std::endl;
 
 	try
 	{
-		Bureaucrat a ( "random", 150 );
-		a.decrementGrade();
+		Bureaucrat c ( "random", 150 );
+		c.decrementGrade();
+		std::cout << "something\n";
 	}
 	catch( std::exception& e )
 	{
-		std::cout << e.what() << "\n\n";
+		std::cout << e.what();
+		NEWLINE
 	}
-
-	// Bureaucrat c ( "", -5 );
 
 	return 0;
 }
