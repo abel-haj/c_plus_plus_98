600b64f47a823eee6248f163551b0c532153c28b
abel-haj <abel-haj@student.1337.ma>
2022-03-29 07:07:56 +0000

    work in progress

---
+++ d07/ex02/Array.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+template < typename T >
+class Array {
+
+	private:
+
+	public:
+		Array <T> ( void ) {
+		}
+
+		Array <T> ( unsigned int size ) {
+		}
+
+		~Array <T> ( void ) {
+		}
+
+		Array <T> & operator = ( Array <T> const & rhs ) {
+
+			return *this;
+		}
+
+};
