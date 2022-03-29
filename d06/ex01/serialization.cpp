4d143e5a013313ff23e29ded543ff6a34253dde4
abel-haj <abel-haj@student.1337.ma>
2022-03-29 02:23:48 +0000

    missing function

--- d06/ex01/serialization.cpp
+++ d06/ex01/serialization.cpp
@@ -1,8 +1,19 @@
 #include "serialization.hpp"
 
-// It takes a pointer and converts it to the unsigned integer type uintptr_t
+/*
+ * Takes a pointer
+ * and converts it to the unsigned integer type uintptr_t
+ */
 uintptr_t serialize(Data* ptr) {
+
 	return reinterpret_cast<uintptr_t>(ptr);
 }
 
+/*
+ * Takes an unsigned integer parameter
+ * and converts it to a pointer to Data
+ */
+Data* deserialize(uintptr_t raw) {
 
+	return reinterpret_cast<Data*>(raw);
+}
