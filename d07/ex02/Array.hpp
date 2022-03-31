f76930ce3e49128f8f24b2524eacebc5eee681c0
abel-haj <abel-haj@student.1337.ma>
2022-03-31 16:09:06 +0000

    finished, need a review

--- d07/ex02/Array.hpp
+++ d07/ex02/Array.hpp
@@ -1,23 +1,96 @@
 #pragma once
 
+#include <exception>
+
 template < typename T >
 class Array {
 
 	private:
+		T *				_actualArray;
+		unsigned int	_size;
 
 	public:
+		/* Default Constructor */
 		Array <T> ( void ) {
+
+			_size = 0;
+			_actualArray = new T [0];
 		}
 
 		Array <T> ( unsigned int size ) {
+
+			_size = size;
+			_actualArray = new T [_size];
+
+			for ( unsigned int i = 0; i < size; i++ ) {
+
+				_actualArray[i] = 0;
+			}
 		}
 
+		/* Copy Constructor */
+		Array <T> ( const Array & src ) {
+
+			this->_size = src.size();
+			this->_actualArray = new T [this->_size];
+			for ( unsigned int i = 0; i < this->_size; i++ ) {
+
+				this->_actualArray[i] = src[i];
+			}
+		}
+
+		/* Destructor */
 		~Array <T> ( void ) {
+
+			delete [] _actualArray;
 		}
 
-		Array <T> & operator = ( Array <T> const & rhs ) {
+		/* Assignment Operator Overload */
+		Array <T> &			operator = ( const Array & rhs ) {
+
+			delete [] _actualArray;
+
+			this->_size = rhs.size();
+			this->_actualArray = new T [this->_size];
+			for ( unsigned int i = 0; i < this->_size; i++ ) {
+
+				this->_actualArray[i] = rhs[i];
+			}
 
 			return *this;
 		}
 
+		/* Subscript Operator Overload (Read) */
+		T				operator [] ( unsigned int index ) const {
+
+			if ( index >= _size || index < 0 ) {
+
+				throw Array::IndexOutOfBoundsException();
+			}
+			return this->_actualArray[index];
+		}
+
+		/* Subscript Operator Overload (Write) */
+		T &				operator [] ( unsigned int index ) {
+
+			if ( index >= _size || index < 0 ) {
+
+				throw Array::IndexOutOfBoundsException();
+			}
+			return this->_actualArray[index];
+		}
+
+		unsigned int	size( void ) const {
+
+			return this->_size;
+		}
+
+		class IndexOutOfBoundsException : public std::exception {
+
+			public:
+				const char * what () const throw () {
+
+					return "Index out of bounds";
+				}
+		};
 };
--- d07/ex02/main.cpp
+++ d07/ex02/main.cpp
@@ -3,5 +3,33 @@
 
 int main( void ) {
 
+	{
+		Array <int> a;
+	}
+
+	{
+		Array < Array < int > > a (5);
+		a[0] = Array <int> (2);
+		a[0][0] = 0;
+		a[0][1] = 1;
+		std::cout << "0:0 -> " << a[0][0] << ",0:1 -> " << a[0][1] << std::endl;
+	}
+
+	{
+		Array <int> a(5);
+		std::cout << "a size is " << a.size() << std::endl;
+		std::cout << "a[0] is " << a[0] << std::endl;
+		a [0] = 5;
+		std::cout << "a[0] is " << a[0] << std::endl;
+		try {
+
+			std::cout << "a[5] is " << a[5] << std::endl;
+
+		} catch ( std::exception & e ) {
+
+			std::cout << "Exception: " << e.what() << std::endl;
+		}
+	}
+
 	return 0;
 }
