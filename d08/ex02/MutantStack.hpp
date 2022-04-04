f10a5ee5d86d1032a3382572f10edcdf5834f40d
abel-haj <abel-haj@student.1337.ma>
2022-04-04 18:35:32 +0000

    finished ex02

--- d08/ex02/MutantStack.hpp
+++ d08/ex02/MutantStack.hpp
@@ -2,18 +2,48 @@
 # define MUTANTSTACK_HPP
 
 # include <stack>
-# include <iostream>
 
 template < typename T >
-class MutantStack : public std::vector<T>
+class MutantStack : public std::stack<T>
 {
-	private:
-
 	public:
+		typedef T*	iterator;
+
 		MutantStack( void ) {}
-		MutantStack( MutantStack const & src ) {}
-		MutantStack operator = ( MutantStack const & src ) {}
+
+		MutantStack( MutantStack const & src ) {
+
+			*this = src;
+		}
+
+		MutantStack & operator = ( MutantStack const & src ) {
+
+			while ( !this->empty() )
+			{
+				this->pop();
+			}
+
+			for (size_t i = 0; i < src.size(); i++)
+			{
+				this->push(src.c[i]);
+			}
+
+			return *this;
+		}
+
 		~MutantStack( void ) {}
+
+		iterator	begin( void ) {
+
+			return &this->top() - this->size() + 1;
+		}
+
+		iterator	end( void ) {
+
+			if (this->size() > 0)
+				return &this->top() + 1;
+			return NULL;
+		}
 };
 
 #endif
--- d08/ex02/main.cpp
+++ d08/ex02/main.cpp
@@ -1,7 +1,86 @@
 #include "MutantStack.hpp"
 #include <iostream>
+#include <vector>
 
 int main( void ) {
+
+	{
+		MutantStack<int> mstack;
+
+		mstack.push(5);
+		mstack.push(17);
+
+		std::cout << mstack.top() << std::endl;
+
+		mstack.pop();
+
+		std::cout << mstack.size() << std::endl;
+		std::cout << "----------" << std::endl;
+
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		mstack.push(0);
+
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+
+		++it;
+		--it;
+
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+
+		std::stack<int> s(mstack);
+		std::cout << "----------" << std::endl;
+
+		while (s.size() > 0)
+		{
+			std::cout << s.top() << std::endl;
+			s.pop();
+		}
+	}
+
+	std::cout << "----------" << std::endl;
+	std::cout << "----------" << std::endl;
+
+	{
+		MutantStack<std::string>ms;
+		MutantStack<std::string>::iterator it;
+
+		ms.push("one");
+		ms.push("two");
+		ms.push("three");
+		ms.push("four");
+
+		for ( it = ms.begin(); it < ms.end(); it++ )
+		{
+			std::cout << *it << std::endl;
+		}
+
+		std::cout << "----------" << std::endl;
+
+		it--;
+		while ( it >= ms.begin() )
+		{
+			std::cout << *it << std::endl;
+			it--;
+		}
+
+		std::cout << "----------" << std::endl;
+
+		MutantStack<std::string>ms2;
+		ms2.push("five");
+		ms2.push("six");
+		ms2 = ms;
+		for ( it = ms2.begin(); it < ms2.end(); it++ )
+		{
+			std::cout << *it << std::endl;
+		}
+	}
 
 	return 0;
 }
